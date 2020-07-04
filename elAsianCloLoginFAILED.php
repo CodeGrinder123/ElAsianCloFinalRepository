201641cafbf9e726e235c54dec2df19c14fcd629
CodeGrinder123 <jberdin@gbox.adnu.edu.ph>
2020-07-04 13:15:02 +0800

    honnorCode

--- elAsianCloLoginFAILED.php
+++ elAsianCloLoginFAILED.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Honor Code:
+
+This is my own work and I have not received any unauthorized help in completing this. 
+I have not copied from my classmate, friend, nor any unauthorized resource. 
+I am well aware of the policies stipulated in the handbook regarding academic dishonesty. 
+If proven guilty, I won't be credited any points for this endeavor.
+*/
 	if(isset($_POST['login'])){
 		header("Location: elAsianClo.php");
 	}
--- elAsianCloSearch.php
+++ elAsianCloSearch.php
@@ -1,5 +1,12 @@
 <?php 
-	
+/*
+Honor Code:
+
+This is my own work and I have not received any unauthorized help in completing this. 
+I have not copied from my classmate, friend, nor any unauthorized resource. 
+I am well aware of the policies stipulated in the handbook regarding academic dishonesty. 
+If proven guilty, I won't be credited any points for this endeavor.
+*/
 	include 'elAsianCloShirts.php';
 	
 	$tShirts = 'elAsianCloShirts.php';
